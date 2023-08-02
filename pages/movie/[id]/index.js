e872eb9603943b9d20bc3a1b658a61d9b5e74b41
magdy mofeed <114363009+magdy222@users.noreply.github.com>
2023-08-03 00:39:48 +0300

    Update index.js

--- pages/movie/[id]/index.js
+++ pages/movie/[id]/index.js
@@ -25,9 +25,12 @@ export async function getStaticPaths(){
     const res = await axios(`${BASE_URL}/popular?api_key=${API_KEY}&language=en-US&page=1`)
     const movies = res.data.results;
 
+    console.log(movies)
 
     const ids = movies.map(movie => movie.id)
     const paths = ids.map(id => ({ params: {id : id.toString()} }))
+
+    console.log(paths);
 
     return{
         paths,
